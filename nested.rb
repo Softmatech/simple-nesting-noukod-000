ed69ab9ef2137776ae8946969ce09987954b2ee0
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-18 17:12:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -61,8 +61,6 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -84,7 +82,7 @@ programmer_hash[:yukihiro_matsumoto] = {
    :languages => ["LISP", "C"]
 }
 
-programmer_hash[:agile_method]
+programmer_hash[:yukihiro_matsumoto]
 end
 
 def changing_alan
