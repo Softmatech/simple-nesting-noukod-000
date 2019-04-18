bfa6176ba58c853e421370ae085b96a28686edbc
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-18 17:09:37 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -79,10 +79,10 @@ def adding_matz
         }
      }
 
-programmer_hash[:agile_method] = [:yukihiro_matsumoto => {
+programmer_hash[:yukihiro_matsumoto] = {
   :known_for => "Ruby",
    :languages => ["LISP", "C"]
-}]
+}
 
 programmer_hash[:agile_method]
 end
